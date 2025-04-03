ad2e92e90ce5a1855ab82e2dc5baae951b30cd93
Juan-BOM <osp21002@byui.edu>
2025-04-03 02:08:36 -0600

    Changed format a bit to make it easier to read

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -15,16 +15,19 @@ class Program
 
         DisplayResult(userName,square);
     }
+    
     static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the Program!");
     }
+
     static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
         string userName = Console.ReadLine();
         return userName;
     }
+
     static int PromptUserNumber()
     {
         Console.Write("What is your favorite number? ");
@@ -32,11 +35,13 @@ class Program
         int favoriteNumber = int.Parse(favoriteNumberString);
         return favoriteNumber;
     }
+
     static int SquareNumber(int favoriteNumber)
     {
         int square = favoriteNumber * favoriteNumber;
         return square;
     }
+
     static void DisplayResult(string userName, int square)
     {
         Console.WriteLine($"{userName}, the square of your number is {square}");
